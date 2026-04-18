28309fc54badc69137798b986f3cb44fda48b21d
stevenstank <stevenstank775@gmail.com>
2026-04-19 00:20:48 +0530

    Phase 1 complete: Project initialized

---
+++ backend/dist/src/index.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    /**
+     * An asynchronous register function that runs before
+     * your application is initialized.
+     *
+     * This gives you an opportunity to extend code.
+     */
+    register({ strapi }) {
+        strapi.server.routes([
+            {
+                method: 'GET',
+                path: '/api',
+                handler(ctx) {
+                    ctx.body = {
+                        status: 'ok',
+                        message: 'Strapi API is running',
+                    };
+                },
+                config: { auth: false },
+            },
+        ]);
+    },
+    /**
+     * An asynchronous bootstrap function that runs before
+     * your application gets started.
+     *
+     * This gives you an opportunity to set up your data model,
+     * run jobs, or perform some special logic.
+     */
+    bootstrap( /* { strapi }: { strapi: Core.Strapi } */) { },
+};
